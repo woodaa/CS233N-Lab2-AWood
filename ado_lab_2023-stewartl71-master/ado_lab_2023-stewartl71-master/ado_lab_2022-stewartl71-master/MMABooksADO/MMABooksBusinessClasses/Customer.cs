6aa1ca507835c1f5fb0dab5b033fdecd23add5c8
woodaa <130096664+woodaa@users.noreply.github.com>
2023-10-10 10:42:54 -0700

    Starting Files

---
+++ ado_lab_2023-stewartl71-master/ado_lab_2023-stewartl71-master/ado_lab_2022-stewartl71-master/MMABooksADO/MMABooksBusinessClasses/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MMABooksBusinessClasses
+{
+    public class Customer
+    {
+        public Customer() { }
+
+        public Customer(int id, string name, string address, string city, string state, string zipcode)
+        {
+            CustomerID = id;
+            Name = name;
+            Address = address;
+            City = city;
+            State = state;
+            ZipCode = zipcode;
+        }
+
+        public int CustomerID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
